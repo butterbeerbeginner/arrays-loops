68071a85b9b1b3e087125ea750372cca1f9340fe
unknown <meredith.marcum@hotmail.com>
2021-07-20 21:42:01 -0500

    Worked on Objects and Arrays

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
 console.log("Showing up is half of the battle!");
+
+
+// Creating our team objects
+
+// secondly fitting all the objects into one array:
+
+const teamInfoArray = [
+
+let teammateOne = {
+  name: "Albert" , 
+  email: "albert.w.chittaphong@gmail.com",
+  favColor: "red" , 
+  id: 9 , 
+  unique: "black belt",
+  image: "https://avatars.githubusercontent.com/u/83558122?v=4"
+},
+
+let teammateTwo = {
+  name: "Craig" , 
+  email: "wellspring.craig@gmail.com",
+  favColor: "purple" , 
+  id: 9 , 
+  unique: "extremely felxible",
+  image: "https://avatars.githubusercontent.com/u/61626143?v=4"
+},
+
+let teammateThree = {
+  name: "Dario" , 
+  email: "darioperez1415@gmail.com",
+  favColor: "Ash Grey" , 
+  id: 3 , 
+  unique: "very good at football",
+  image: "https://avatars.githubusercontent.com/u/83309084?v=4"
+}
+
+]
